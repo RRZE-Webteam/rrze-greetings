eb6caca1c120cff70aa2e9dc0435301516a1f559
rvdforst <rolf.v.d.forst@fau.de>
2020-11-24 16:14:30 +0100

    Create Template.php

---
+++ includes/Template.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RRZE\Greetings;
+
+defined('ABSPATH') || exit;
+
+/**
+ * [Template description]
+ */
+class Template
+{
+    /**
+     * [__construct description]
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * [getContent description]
+     * @param  string $template [description]
+     * @param  array  $data     [description]
+     * @return string           [description]
+     */
+    public function getContent($template = '', $data = [])
+    {
+        return $this->parseContent($template, $data);
+    }
+
+    /**
+     * [parseContent description]
+     * @param  string $template [description]
+     * @param  array  $data     [description]
+     * @return string           [description]
+     */
+    protected function parseContent($template, $data)
+    {
+        $templateFile = $this->getTemplate($template);
+        if (!$templateFile || empty($data)) {
+            return '';
+        }
+        $parser = new Parser();
+        return $parser->parse($templateFile, $data);
+    }
+
+    /**
+     * [getTemplate description]
+     * @param  string $template [description]
+     * @return string           [description]
+     */
+    protected function getTemplate($template)
+    {
+        $extension = pathinfo($template, PATHINFO_EXTENSION) ? '' : '.html';
+        $templateFile = sprintf('%s%s%s', plugin()->getDirectory(), $template, $extension);
+        return is_readable($templateFile) ? $templateFile : '';
+    }
+}
