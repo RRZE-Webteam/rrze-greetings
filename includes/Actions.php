cc21221c6dabbe54018fb1856f55545ce2cdcd8f
rvdforst <rolf.v.d.forst@fau.de>
2020-12-04 14:08:30 +0100

    Update Actions.php

--- includes/Actions.php
+++ includes/Actions.php
@@ -50,7 +50,6 @@ class Actions
 				case 'send':
 					if (!Queue::isQueueBeingCreated($postId)) {
 						Greeting::setStatus($postId, 'send');
-						delete_post_meta($postId, 'rrze_greetings_card_id');
 						$transientData->addData('success', __('Emails have been sent to the mail queue.', 'rrze-greetings'));
 					} else {
 						$transientData->addData('error', __('Unable to send the emails to the mail queue.', 'rrze-greetings'));
